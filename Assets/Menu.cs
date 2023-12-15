68227e362d8cef6a1903e9baec3aa435c258ee60
AmeeshBains <B00146816@mytudublin.ie>
2023-12-15 17:46:42 +0000

    GUN

---
+++ Assets/Menu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+
+    public void easy()
+    {
+
+        SceneManager.LoadSceneAsync(1);
+
+
+    }
+
+    public void medium()
+    {
+
+        SceneManager.LoadSceneAsync(2);
+
+
+    }
+
+
+
+    public void hard()
+    {
+
+        SceneManager.LoadSceneAsync(3);
+
+
+    }
+
+
+
+
+    public void OpenUrl()
+    {
+
+        Application.OpenURL("https://github.com/Ameeshbains/GunsPhobia.git");
+
+
+    }
+
+
+
+
+
+}
